084fdf3fc005769c1d9959e66440c4f96450a2a4
Maksim Alyabyshev <truepatch@gmail.com>
2022-04-29 01:45:50 +0500

    gh pages added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,10 @@ function Footer() {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="#!">
+                    <a
+                        className="grey-text text-lighten-4 right"
+                        href="https://truepatch.github.io/react-omdb-movies"
+                    >
                         Repo
                     </a>
                 </div>
